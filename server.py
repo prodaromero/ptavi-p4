151fe8c6a17d26a598b4c52b98622c3da0f0fa19
Pablo Roda Romero <proda@alpha10.aulas.gsyc.urjc.es>
2016-10-24 19:56:55 +0200

    Pep9 pasado

--- server.py
+++ server.py
@@ -9,26 +9,41 @@ import sys
 import json
 import time
 
+
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
-    Echo server class 
+    Echo server class
     """
     dicc = {}
     attr = {}
+    Time = time.gmtime(time.time())
+
+    def json2registered(self):
+        """
+        COMPROBAMOS SI HAY FICHERO JSON
+        """
+        try:
+            file = open('registered.json', 'r')
+            self.dicc = file.load(file)
+        except:
+            self.dicc = {}
 
     def register2json(self):
+
+        """
+        CREAMOS ARCHIVO JSON
+        """
         file = open('registered.json', 'w')
-        json.dump(self.dicc, file, sort_keys=True, 
+        json.dump(self.dicc, file, sort_keys=True,
                   indent=4, separators=(',', ':'))
         file.close()
 
     def handle(self):
-        
+
         print("(IP, PORT):" + str(self.client_address))
         line = self.rfile.read()
         message = line.decode('utf-8')
         print("El cliente nos manda " '\r\n' + message)
-            
 
         if message.split(' ')[0] == "REGISTER":
             user = message.split(' ')[1]
@@ -36,8 +51,8 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             expires = message.split(' ')[3]
             expires = expires.split('\r\n')[0]
             time_expires = time.gmtime(int(expires))
-            time_expires = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
-
+            time_expires = time.strftime('%Y-%m-%d %H:%M:%S',
+                                         time.gmtime(time.time()))
             if int(expires) == 0:
                 if user in self.dicc:
                     del self.dicc[user]
@@ -48,6 +63,12 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 self.attr['expires'] = time_expires
                 self.dicc[user] = self.attr
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            try:
+                attr = self.dicc[user]
+                if Time >= time.strptime(attr['expires'], '%Y-%m-%d %H:%M:%S'):
+                    del self.dicc[user]
+            except:
+                pass
 
             print(self.dicc)
 
