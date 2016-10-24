64e2e6745b4557230508176d6279a3f934471543
Pablo Roda Romero <proda@alpha10.aulas.gsyc.urjc.es>
2016-10-24 19:54:27 +0200

    pep 8

--- client.py
+++ client.py
@@ -7,9 +7,8 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-
 try:
-# Constantes. Dirección IP del servidor y contenido a enviar
+    # Constantes. Dirección IP del servidor y contenido a enviar
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
     LINE = ' '.join(sys.argv[3:4])
@@ -24,7 +23,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     if sys.argv[3] == 'register':
         user = str(sys.argv[4])
         expires = str(EXPIRES)
-        message = ('REGISTER '+'sip:'+user+' SIP/2.0\r\n'+'Expires: '+expires+'\r\n')
+        message = ('REGISTER ' + 'sip:' + user + ' SIP/2.0\r\n' + 'Expires: ' +
+                   expires + '\r\n')
         my_socket.send(bytes(message, 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
