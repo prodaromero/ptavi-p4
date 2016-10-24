d2e95b7951cc3e8f43832e9d2b835b208b894f3b
Pablo Roda Romero <proda@gamma24.aulas.gsyc.urjc.es>
2016-10-24 18:39:55 +0200

    punto 8 acabado

--- server.py
+++ server.py
@@ -6,15 +6,24 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
-
+import json
+import time
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class 
     """
     dicc = {}
+    attr = {}
+
+    def register2json(self):
+        file = open('registered.json', 'w')
+        json.dump(self.dicc, file, sort_keys=True, 
+                  indent=4, separators=(',', ':'))
+        file.close()
 
     def handle(self):
+        
         print("(IP, PORT):" + str(self.client_address))
         line = self.rfile.read()
         message = line.decode('utf-8')
@@ -26,17 +35,23 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             user = user.split(':')[1]
             expires = message.split(' ')[3]
             expires = expires.split('\r\n')[0]
+            time_expires = time.gmtime(int(expires))
+            time_expires = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+
             if int(expires) == 0:
                 if user in self.dicc:
                     del self.dicc[user]
                 else:
                     print("ERROR: this user dont exist")
             else:
-                self.dicc[user] = self.client_address[0]
-
-            print("SIP/2.0 200 OK" + '\r\n\r\n')
+                self.attr['address'] = self.client_address[0]
+                self.attr['expires'] = time_expires
+                self.dicc[user] = self.attr
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
             print(self.dicc)
+
+        self.register2json()
 
 if __name__ == "__main__":
     try:
