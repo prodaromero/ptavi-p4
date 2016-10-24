741222514fa4f5918477d0275b09ae18b0a24796
Pablo Roda Romero <proda@gamma24.aulas.gsyc.urjc.es>
2016-10-24 16:53:31 +0200

    client arreglado

--- client.py
+++ client.py
@@ -7,19 +7,26 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-LINE = ' '.join(sys.argv[3:])
 
-       
+try:
+# Constantes. Dirección IP del servidor y contenido a enviar
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    LINE = ' '.join(sys.argv[3:4])
+    EXPIRES = sys.argv[5]
+except:
+    sys.exit("ERROR: introduce SERVER, PORT and USER")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
+    if sys.argv[3] == 'register':
+        user = str(sys.argv[4])
+        expires = str(EXPIRES)
+        message = ('REGISTER '+'sip:'+user+' SIP/2.0\r\n'+'Expires: '+expires+'\r\n')
+        my_socket.send(bytes(message, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
